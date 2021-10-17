e0530ad67925bdddac37a9af9ef6018ba9b1ce28
CollinFunaki <collinf@vt.edu>
2021-10-17 12:36:47 -0400

    added pokemon service log

--- p2refactorbackend/src/main/java/com/revature/services/PokemonService.java
+++ p2refactorbackend/src/main/java/com/revature/services/PokemonService.java
@@ -2,17 +2,24 @@ package com.revature.services;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.revature.daos.PokemonDao;
 import com.revature.models.Pokemon;
 
 public class PokemonService {
 	
+	Logger log = LogManager.getLogger(); //Logger object so that we can implement Logging
+	
 	PokemonDao pd = new PokemonDao();
 
 	public boolean insertAllPokemon(int trainerId, Pokemon pokemon) {
 		if(pd.insertPokemon(trainerId, pokemon)) {
+			log.info("Trainer caught (and inserted) a Pokemon");
 			return true;
 		}
+		log.warn("Pokemon insert failed");
 		return false;
 	}
 
