3af6cfaf8dfbb61d53599f2ed3571b43bf79cc03
CollinFunaki <collinf@vt.edu>
2021-10-16 22:49:26 -0400

    added login service and controller from tristan

--- p2refactorbackend/src/main/java/com/revature/services/InventoryService.java
+++ p2refactorbackend/src/main/java/com/revature/services/InventoryService.java
@@ -1,5 +1,9 @@
 package com.revature.services;
 
 public class InventoryService {
+	
+	InventoryService iDao = new InventoryService();
+	
+	
 
 }
--- p2refactorbackend/src/main/java/com/revature/services/LoginService.java
+++ p2refactorbackend/src/main/java/com/revature/services/LoginService.java
@@ -5,31 +5,31 @@ import com.revature.daos.TrainerDao;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class LoginService {
-	
-	private static LoginService ls = null;
-	
-	private LoginService() {
-		//no implementation
-	}
-	
-	
-	
-	public boolean login(String username, String password) {
-		TrainerDao tDao = TrainerDao.getTrainerDao();
-		//BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
-		//String passwordHash = encoder.encode(password);
-		if(tDao.getForLogin(username, password/*Hash*/).size() != 1) {
-			return false;
-		}
-		return true;
-	}
 
-	public static LoginService getLoginService() {
-		if(ls == null) {
-			ls = new LoginService();
-		}
-		
-		return ls;
-	}
-	
+    private static LoginService ls = null;
+
+    private LoginService() {
+        //no implementation
+    }
+
+
+
+    public boolean login(String username, String password) {
+        TrainerDao tDao = TrainerDao.getTrainerDao();
+        //BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
+        //String passwordHash = encoder.encode(password);
+        if(tDao.getForLogin(username, password).size() != 1) {
+            return false;
+        }
+        return true;
+    }
+
+    public static LoginService getLoginService() {
+        if(ls == null) {
+            ls = new LoginService();
+        }
+
+        return ls;
+    }
+
 }
