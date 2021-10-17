5f6cd873d82e513399018ec6d2cafe7c28732e62
bgarber0343 <bgarber0343@gmail.com>
2021-10-16 21:20:14 -0500

    tweaked login service and user model

--- p2-group3-front-end/src/app/models/user.ts
+++ p2-group3-front-end/src/app/models/user.ts
@@ -1,5 +1,7 @@
 export class User {
-    id: number;
-    username: string;
-    password: string;
+    constructor(
+        public userId:number,
+        public username:string,
+        public password:string,
+    ) { }
 }
