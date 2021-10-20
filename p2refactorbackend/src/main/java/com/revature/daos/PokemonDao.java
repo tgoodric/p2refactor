fb0d4f45a0a6bac5b4e91e394d2bc8e251acd510
Nabin Khatri <submergednabin@gmail.com>
2021-10-19 21:43:06 -0400

    Pokemon inventory daos and controller exception handling done

--- p2refactorbackend/src/main/java/com/revature/daos/PokemonDao.java
+++ p2refactorbackend/src/main/java/com/revature/daos/PokemonDao.java
@@ -2,6 +2,7 @@ package com.revature.daos;
 
 import java.util.List;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
 import com.revature.models.Pokemon;
@@ -13,28 +14,42 @@ public class PokemonDao implements IPokemonDao {
 	@Override
 	@SuppressWarnings("unchecked")
 	public List<Pokemon> getPokemon(int trainerId) {
-		Session ses = HibernateUtil.getSession();
-		String HQL = "FROM Pokemon WHERE trainer_id=:tID";
-		List<Pokemon> allPokemon = ses.createQuery(HQL)
-				.setParameter("tID", trainerId)
-				.list();
-		HibernateUtil.closeSession();
-		return allPokemon;
+		
+		try(Session ses = HibernateUtil.getSession()) {
+			
+			String HQL = "FROM Pokemon WHERE trainer_id=:tID";
+			List<Pokemon> allPokemon = ses.createQuery(HQL)
+					.setParameter("tID", trainerId)
+					.list();
+			HibernateUtil.closeSession();
+			return allPokemon;
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			return (List<Pokemon>) e;
+		}
+		
+		
 	}
 
 	@Override
 	@SuppressWarnings("unchecked")
 	public List<Pokemon> getPokemonUpToLevel(int trainerId, int level) {
+		try(Session ses = HibernateUtil.getSession()){
+			String Hql = "FROM Pokemon WHERE trainer_id=:tID and level=:lvl ";
+			List<Pokemon> listPokeLevel = ses.createQuery(Hql)
+					.setParameter("tID", trainerId)
+					.setParameter("lvl", level)
+					.list();
+			HibernateUtil.closeSession();
+			return listPokeLevel;
+
+		}catch(HibernateException e) {
+			
+			e.printStackTrace();
+			return (List<Pokemon>) e;
+		}
 		
-		Session ses = HibernateUtil.getSession();
-		String Hql = "FROM Pokemon WHERE trainer_id=:tID and level=:lvl ";
-		List<Pokemon> listPokeLevel = ses.createQuery(Hql)
-				.setParameter("tID", trainerId)
-				.setParameter("lvl", level)
-				.list();
-		HibernateUtil.closeSession();
-		return listPokeLevel;
-	}
+}
 
 	@Override
 	public boolean addPokemon(int trainerId, int pokedexNumber) {
@@ -52,22 +67,32 @@ public class PokemonDao implements IPokemonDao {
 	@Override
 	public boolean insertPokemon(int trainerId, Pokemon pokemon) {
 		
-		Session ses = HibernateUtil.getSession();
-		
 		//First we need to match trainerId from Trainer Model in Hibernate
-		//Query for for finding trainer ID
-		String hql = "FROM Trainer where trainer_id = :tID";
-		Trainer trainer = (Trainer) ses.createQuery(hql)
-				.setParameter("tID", trainerId)
-				.uniqueResult();
+				//Query for for finding trainer ID
 		
-		//we are setting trainerID from ^^ above in Pokemon Foreignkey TrainerId column
-		pokemon.setTrainerIdFk(trainer);
+		try(Session ses = HibernateUtil.getSession()){
+			
+			String hql = "FROM Trainer where trainer_id = :tID";
+			Trainer trainer = (Trainer) ses.createQuery(hql)
+					.setParameter("tID", trainerId)
+					.uniqueResult();
+			
+			//we are setting trainerID from ^^ above in Pokemon Foreignkey TrainerId column
+			pokemon.setTrainerIdFk(trainer);
+			
+			ses.save(pokemon);
+			
+			HibernateUtil.closeSession();
+			return true;
+			
+		}catch(HibernateException e) {
+			e.printStackTrace();
+			return false;
+			
+		}
 		
-		ses.save(pokemon);
 		
-		HibernateUtil.closeSession();
-		return true;
+		
 		
 	}
 
