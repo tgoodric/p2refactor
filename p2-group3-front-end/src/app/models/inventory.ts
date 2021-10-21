d6188b24838b1f572fc369aadbf5179c3f1f4af3
bgarber0343 <bgarber0343@gmail.com>
2021-10-21 15:24:27 -0500

    added useItem/addItem to pokemon.service.ts

---
+++ p2-group3-front-end/src/app/models/inventory.ts
@@ -0,0 +1,9 @@
+export class Inventory {
+    constructor(
+        public id:number,
+        public pokeballs:number,
+        public potions:number,
+        public superPotions:number,
+        public trainerId:number
+    ) { }
+}
